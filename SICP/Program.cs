f1df2d63e44d63cd37084d3dc24709704f685d3a
Carl Björknäs <carl.bjorknas@gmail.com>
2023-03-18 15:18:57 +0100

    Adds input loop.

--- SICP/Program.cs
+++ SICP/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using SICP;
+
+Console.WriteLine("Start coding!");
+Console.WriteLine("Quit by entering 'q'.");
+
+var engine = new Engine();
+var code = Console.ReadLine();
+
+// TODO Change to ctrl-c?
+while(code != "q")
+{
+    try
+    {
+        var result = engine.Eval(code);
+        Console.WriteLine(result);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    } 
+    
+    code = Console.ReadLine();
+}
+
 
