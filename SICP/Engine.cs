3611525d67835325a45af6f195845400dfeb247d
Carl Björknäs <carl.bjorknas@gmail.com>
2023-03-19 07:55:05 +0100

    Upgraded projects to .Net 7.

--- SICP/Engine.cs
+++ SICP/Engine.cs
@@ -1,112 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SICP;
 
-namespace SICP
+public class Engine
 {
-    public class Engine
+    public string Eval(string code)
     {
-        public string Eval(string code)
+        if (code.StartsWith("("))
         {
-            if (code.StartsWith("("))
+            if (!code.EndsWith(")"))
+                throw new Exception($"Expression did not end with a closing parenthesis. '{code}'");
+
+            var undressedEpxr = code.Substring(1, code.Length - 2);
+            var op = GetOperator(undressedEpxr);
+            var operands = GetOperands(undressedEpxr).ToList();
+            operands = operands.Select(Eval).ToList();
+
+            if (op == '+')
+                return operands.Select(x => int.Parse(x)).Sum().ToString();
+            else if (op == '-')
             {
-                if (!code.EndsWith(")"))
-                    throw new Exception($"Expression did not end with a closing parenthesis. '{code}'");
+                if (!operands.Any())
+                    return 0.ToString();
 
-                var undressedEpxr = code.Substring(1, code.Length - 2);
-                var op = GetOperator(undressedEpxr);
-                var operands = GetOperands(undressedEpxr).ToList();
-                operands = operands.Select(Eval).ToList();
+                if (operands.Count == 1)
+                    return (-int.Parse(operands[0])).ToString();
 
-                if (op == '+')
-                    return operands.Select(x => int.Parse(x)).Sum().ToString();
-                else if (op == '-')
-                {
-                    if (!operands.Any())
-                        return 0.ToString();
-
-                    if (operands.Count == 1)
-                        return (-int.Parse(operands[0])).ToString();
-
-                    var sum = operands.Skip(1).Select(int.Parse).Sum();                    
-                    return (int.Parse(operands[0]) - sum).ToString();
-                }
+                var sum = operands.Skip(1).Select(int.Parse).Sum();                    
+                return (int.Parse(operands[0]) - sum).ToString();
             }
-
-            return code;
         }
 
-        private char GetOperator(string code)
+        return code;
+    }
+
+    private char GetOperator(string code)
+    {
+        if (code.Length >= 1)
+            return code[0];
+
+        throw new Exception($"Could not find an operator in '{code}'.");
+    }
+
+    /// <summary>
+    /// Outermost parentheses has been removed.
+    /// Examples:
+    /// + 1 2
+    /// + (+ 1 2) 3
+    /// </summary>
+    /// <param name="undressedCode"></param>
+    /// <returns></returns>
+    private IEnumerable<string> GetOperands(string undressedCode)
+    {
+        if (undressedCode.Length == 1)
+            yield break;
+
+        var rest = undressedCode.Substring(2);
+        while (rest.Any())
         {
-            if (code.Length >= 1)
-                return code[0];
-
-            throw new Exception($"Could not find an operator in '{code}'.");
-        }
-
-        /// <summary>
-        /// Outermost parentheses has been removed.
-        /// Examples:
-        /// + 1 2
-        /// + (+ 1 2) 3
-        /// </summary>
-        /// <param name="undressedCode"></param>
-        /// <returns></returns>
-        private IEnumerable<string> GetOperands(string undressedCode)
-        {
-            if (undressedCode.Length == 1)
-                yield break;
-
-            var rest = undressedCode.Substring(2);
-            while (rest.Any())
+            if (rest[0] == '(')
             {
-                if (rest[0] == '(')
+                var index = FindIndexOfMatchingClosingParen(rest);
+                yield return rest[..(index + 1)];
+                rest = rest[(index + 1)..];
+            } 
+            else if (rest[0] == ' ')
+            {
+                rest = rest[1..];
+            }
+            else
+            {
+                var index = rest.IndexOf(' ');
+                if (index == -1)
                 {
-                    var index = FindIndexOfMatchingClosingParen(rest);
-                    yield return rest[..(index + 1)];
-                    rest = rest[(index + 1)..];
-                } 
-                else if (rest[0] == ' ')
-                {
-                    rest = rest[1..];
+                    yield return rest;
+                    yield break;
                 }
+
+                var operand = rest[..index];
+                rest = rest[(index + 1)..];
+                yield return operand;
+            }
+        }
+    }
+
+    private int FindIndexOfMatchingClosingParen(string operandsString)
+    {
+        var numberUnclosed = 1;
+        for (int i = 1; i < operandsString.Length; i++)
+        {
+            if (operandsString[i] == ')')
+                if (numberUnclosed == 1)
+                    return i;
                 else
                 {
-                    var index = rest.IndexOf(' ');
-                    if (index == -1)
-                    {
-                        yield return rest;
-                        yield break;
-                    }
-
-                    var operand = rest[..index];
-                    rest = rest[(index + 1)..];
-                    yield return operand;
+                    numberUnclosed--;
+                    if (numberUnclosed == 0)
+                        throw new Exception($"Too many closing parens in '{operandsString}'");
                 }
-            }
+            else if (operandsString[i] == '(')
+                numberUnclosed++;
         }
 
-        private int FindIndexOfMatchingClosingParen(string operandsString)
-        {
-            var numberUnclosed = 1;
-            for (int i = 1; i < operandsString.Length; i++)
-            {
-                if (operandsString[i] == ')')
-                    if (numberUnclosed == 1)
-                        return i;
-                    else
-                    {
-                        numberUnclosed--;
-                        if (numberUnclosed == 0)
-                            throw new Exception($"Too many closing parens in '{operandsString}'");
-                    }
-                else if (operandsString[i] == '(')
-                    numberUnclosed++;
-            }
-
-            throw new Exception($"Found no matching closing parenthesis in '{operandsString}'");
-        }
+        throw new Exception($"Found no matching closing parenthesis in '{operandsString}'");
     }
 }
--- SICP_Tests/EngineTests.cs
+++ SICP_Tests/EngineTests.cs
@@ -2,89 +2,88 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SICP;
 
-namespace SICP_Tests
+namespace SICP_Tests;
+
+[TestClass]
+public class EngineTests
 {
-    [TestClass]
-    public class EngineTests
+    [TestMethod]
+    public void When_given_a_number_the_number_is_returned()
     {
-        [TestMethod]
-        public void When_given_a_number_the_number_is_returned()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("6");
-            result.Should().Be("6");
-        }
+        var sut = new Engine();
+        var result = sut.Eval("6");
+        result.Should().Be("6");
+    }
 
-        [TestMethod]
-        public void Addition_without_arguments_returns_0()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(+)");
-            result.Should().Be("0");
-        }
+    [TestMethod]
+    public void Addition_without_arguments_returns_0()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(+)");
+        result.Should().Be("0");
+    }
 
-        [TestMethod]
-        public void When_given_an_addition_with_two_numbers_their_sum_is_returned()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(+ 1 2)");
-            result.Should().Be("3");
-        }
+    [TestMethod]
+    public void When_given_an_addition_with_two_numbers_their_sum_is_returned()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(+ 1 2)");
+        result.Should().Be("3");
+    }
 
-        [TestMethod]
-        public void Addition_where_the_only_operand_is_an_addition_expression()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(+ (+ 1 2))");
-            result.Should().Be("3");
-        }
+    [TestMethod]
+    public void Addition_where_the_only_operand_is_an_addition_expression()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(+ (+ 1 2))");
+        result.Should().Be("3");
+    }
 
-        [TestMethod]
-        public void Addition_with_many_levels_of_expression_nesting()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(+ (+ (+ (+ (+ 1 2)))))");
-            result.Should().Be("3");
-        }
+    [TestMethod]
+    public void Addition_with_many_levels_of_expression_nesting()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(+ (+ (+ (+ (+ 1 2)))))");
+        result.Should().Be("3");
+    }
 
-        [TestMethod]
-        public void Subtraction_without_arguments_returns_0()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(-)");
-            result.Should().Be("0");
-        }
+    [TestMethod]
+    public void Subtraction_without_arguments_returns_0()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(-)");
+        result.Should().Be("0");
+    }
 
-        [TestMethod]
-        public void When_given_a_subtraction_with_two_numbers_their_diff_is_returned()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(- 2 1)");
-            result.Should().Be("1");
-        }
+    [TestMethod]
+    public void When_given_a_subtraction_with_two_numbers_their_diff_is_returned()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(- 2 1)");
+        result.Should().Be("1");
+    }
 
-        [TestMethod]
-        public void When_given_a_unary_subtraction_of_a_positive_number_it_is_returned_with_a_minus()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(- 2)");
-            result.Should().Be("-2");
-        }
+    [TestMethod]
+    public void When_given_a_unary_subtraction_of_a_positive_number_it_is_returned_with_a_minus()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(- 2)");
+        result.Should().Be("-2");
+    }
 
-        [TestMethod]
-        public void When_given_a_subtraction_with_three_numbers_the_first_is_reduced_by_the_two_others()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(- 10 3 4)");
-            result.Should().Be("3");
-        }
+    [TestMethod]
+    public void When_given_a_subtraction_with_three_numbers_the_first_is_reduced_by_the_two_others()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(- 10 3 4)");
+        result.Should().Be("3");
+    }
 
-        [TestMethod]
-        public void Math_operation_with_operands_where_some_are_numbers_and_some_are_expressions()
-        {
-            var sut = new Engine();
-            var result = sut.Eval("(+ 1 (+ 2 3) (- 4 1) 2)");
-            result.Should().Be((1 + 5 + 3 + 2).ToString());
-        }
+    [TestMethod]
+    public void Math_operation_with_operands_where_some_are_numbers_and_some_are_expressions()
+    {
+        var sut = new Engine();
+        var result = sut.Eval("(+ 1 (+ 2 3) (- 4 1) 2)");
+        result.Should().Be((1 + 5 + 3 + 2).ToString());
     }
 }
